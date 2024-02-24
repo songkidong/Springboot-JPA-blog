1f34056cefad5db0920bbcada1be93c9e532037a
songkidong <ehdzl5464@naver.com>
2024-02-24 19:16:04 +0900

    http 요청 실습

---
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -0,0 +1,44 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 사용자가 요청 -> 응답(HTML 파일)
+// @Controller
+
+// 사용자가 요청 -> 응답(Data)
+@RestController
+@RequestMapping("/http")
+public class HttpControllerTest {
+
+	// localhost:8080/http/get (select)
+	// 인터넷 브라우저 요청은 무조건 get 요청밖에 할 수 없다.
+	// get 방식으로 data를 요청할 때 쿼리스트링 방법 뿐
+	@GetMapping("/get")
+	public String getTest(Member m) {
+		return "get 요청 :" + m.getId() + m.getUsername() + m.getPassword() + m.getEmail();
+	}
+
+	// localhost:8080/http/post (insert)
+	@PostMapping("/post") // text/plain, application/json
+	public String postTest(@RequestBody Member m) { // MessageConverter
+		return "post 요청" + m.getId() + m.getUsername() + m.getPassword() + m.getEmail();
+	}
+
+	// localhost:8080/http/put (update)
+	@PutMapping("/put")
+	public String putTest(@RequestBody Member m) {
+		return "put 요청"+ m.getId() + m.getUsername() + m.getPassword() + m.getEmail();
+	}
+
+	// localhost:8080/http/delete (delete)
+	@DeleteMapping("/delete")
+	public String deleteTest() {
+		return "delete 요청";
+	}
+}
