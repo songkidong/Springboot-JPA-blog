14d5971d465766ca1fbff29f92c1d86199ebe21a
songkidong <ehdzl5464@naver.com>
2024-03-03 16:41:12 +0900

    회원가입을 위한 enum 사용법

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,10 +4,11 @@ import java.sql.Timestamp;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.DynamicInsert;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,25 +23,27 @@ import lombok.NoArgsConstructor;
 @Builder // 빌더 패턴
 //ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity // User 클래스가 MySQL에 테이블 생성이 된다.
-@DynamicInsert
+//@DynamicInsert // insert 시에 null인 필드를 제외시켜준다.
 public class User {
 
 	@Id // Primary Key
-	@GeneratedValue(strategy=GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링전략을 따라간다.
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링전략을 따라간다.
 	private int id; // 시퀀스, auto_increment
-	
-	@Column(nullable=false, length=30)
+
+	@Column(nullable = false, length = 30)
 	private String username;
-	
-	@Column(nullable=false, length=100) // 해쉬(비밀번호 암호화)
+
+	@Column(nullable = false, length = 100) // 해쉬(비밀번호 암호화)
 	private String password;
-	
-	@Column(nullable=false, length=50)
+
+	@Column(nullable = false, length = 50)
 	private String email;
-	
-	@ColumnDefault("'user'")
-	private String role; // Enum을 쓰는게 좋다. // admin,user,manager
-	
+
+//	@ColumnDefault("user")
+	// DB는 RoleType이라는게 없다.
+	@Enumerated(EnumType.STRING)
+	private RoleType role; // Enum을 쓰는게 좋다. // ADMIN, USER
+
 	@CreationTimestamp // 시간이 자동 입력
 	private Timestamp createDate;
 }
--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -12,7 +13,7 @@ public class DummyControllerTest {
 
 	@Autowired // 의존성 주입
 	private UserRepository userRepository;
-	
+
 	// http://localhost:8000/blog/dummy/join
 	// http 바디에 username, password, email 데이터를 가지고 (요청)
 	@PostMapping("/dummy/join")
@@ -23,7 +24,8 @@ public class DummyControllerTest {
 		System.out.println("email : " + user.getEmail());
 		System.out.println("role : " + user.getRole());
 		System.out.println("createDate : " + user.getCreateDate());
-		
+
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return "회원가입이 완료되었습니다.";
 	}
